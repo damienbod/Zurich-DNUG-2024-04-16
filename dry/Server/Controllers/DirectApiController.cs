de5204cdfb34a349b7163ebad486c41fc5b6b50e
damienbod <damien_bod@hotmail.com>
2024-04-15 14:13:46 +0200

    Init repo

---
+++ dry/Server/Controllers/DirectApiController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dry.Server.Controllers;
+
+[ValidateAntiForgeryToken]
+[Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Policy = "p-admin-or-user")]
+[ApiController]
+[Route("api/[controller]")]
+public class DirectApiController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new List<string> { "some data", "more data", "loads of data" };
+    }
+}
---
+++ dry/Server/UserAdminHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace dry.Server;
+
+public class UserAdminHandler : AuthorizationHandler<UserAdminRequirement>
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, UserAdminRequirement requirement)
+    {
+        var userClaim = context.User.FindFirst(c => c.Type == "roles" && c.Value == "user");
+        var adminClaim = context.User.FindFirst(c => c.Type == "roles" && c.Value == "admin");
+
+        if (userClaim is null && adminClaim is null)
+        {
+            return Task.CompletedTask;
+        }
+
+        context.Succeed(requirement);
+        
+        return Task.CompletedTask;
+    }
+}
